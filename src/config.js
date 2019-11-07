060ad3836eed362c70965fdb2db11cec7597391f
jvymvn <jordon.carter@yahoo.com>
2019-11-07 00:55:40 -0500

    Api fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     //`http://localhost:8000/api`
     // `https://mysterious-tor-25984.herokuapp.com/api`
-    API_ENDPOINT: `http://localhost:8000/api`,
+    API_ENDPOINT: `https://mysterious-tor-25984.herokuapp.com/api`,
     JWT_TOKEN: process.env.REACT_APP_JWT_TOKEN || 'JWT_TOKEN',
 }
